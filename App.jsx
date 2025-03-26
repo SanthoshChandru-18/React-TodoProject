936e4b88616643892c5e71894dd907ff1fcb458f
SanthoshChandru-18 <santhosh18chandru@gmail.com>
2025-03-26 11:51:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Calculator from './Components/Calculator';
+import ToggleBackgroundColor from './Components/ToggleBackgroundColor';
+// import Counter from './Components/Counter'
+// import Todo from './Components/Todo';
+// import Meals from './Components/Meals';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter /> */}
+      {/* <Todo /> */}
+      {/* <Meals/> */}
+      {/* <Calculator /> */}
+      <ToggleBackgroundColor />
+    </div>
+  )
+}
+
+export default App;
