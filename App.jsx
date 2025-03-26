f5a7b2a6cca9ef6d62c0386a02c7e08c30035393
SanthoshChandru-18 <santhosh18chandru@gmail.com>
2025-03-26 11:53:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-// import Calculator from './Components/Calculator';
-import ToggleBackgroundColor from './Components/ToggleBackgroundColor';
-// import Counter from './Components/Counter'
 // import Todo from './Components/Todo';
-// import Meals from './Components/Meals';
 
 const App = () => {
   return (
     <div>
-      {/* <Counter /> */}
-      {/* <Todo /> */}
-      {/* <Meals/> */}
-      {/* <Calculator /> */}
-      <ToggleBackgroundColor />
+      <Todo />
     </div>
   )
 }
